802a7fe3294bdb7b5a5d8dadbf2b3e0b24b6e5f1
MengyuanZhu <zhumengyuan@outlook.com>
2017-03-15 14:19:57 -0400

    update

--- autoRR.py
+++ autoRR.py
@@ -5,11 +5,21 @@ from openeye.oechem import *
 from openeye.oeomega import *
 import sys
 
-def main(argv=[__name__]):
+pkaLib={}
 
-	if len(argv) != 3:
-		raise ValueError("%s <infile> <outfile>" % argv[0])
+libraryFile=["library/aromatic.lib","library/hydrophobic.lib","library/polar_negative.lib","library/polar_positive.lib","library/polar_uncharged.lib"]
 
+def preparepKa():
+	pkafile=open("pka.lib","r")
+	line=pkafile.readline()
+	while True:
+		if not line:
+			break
+		pkaItem=line.split(" ")
+		pkaLib[pkaItem[0]]=pkaItem[1]
+		line=pkafile.readline()
+
+def prepareAromatic():
 	#Generate aromatic library
 	#It will read linker, end and complex to generate aromatic.lib
 	arolink=file("library/aromatic-linker.lib","r")
@@ -39,40 +49,27 @@ def main(argv=[__name__]):
 			break
 		aro.write(line)
 		line=arocom.readline()
-	#end of process aromatic
 
-
-
-	m=Chem.MolFromMol2File("example.mol2")
-	patt=Chem.MolFromSmarts("[Au]")
-	filename="result"
-	file1="library/aromatic.lib"
-	file2="library/hydrophobic.lib"
-	file3="library/polar_negative.lib"
-	file4="library/polar_positive.lib"
-	file5="library/polar_uncharged.lib"
-
-
-	f=open(file2,"r")
-
-	pkafile=open("pka.lib","r")
-
-	line=pkafile.readline()
-	pkaLib={}
-
-	while True:
-		if not line:
-			break
-		pkaItem=line.split(" ")
-		pkaLib[pkaItem[0]]=pkaItem[1]
-		line=pkafile.readline()
-
-	line=f.readline()
-	i=1
+def main(argv=[__name__]):
+	
+	if len(argv) != 3:
+		raise ValueError("%s <infile> <outfile>" % argv[0])
 
 	ofs = oemolostream()
-	if not ofs.open(argv[1]):
-		OEThrow.Fatal("Unable to open %s for writing" % argv[1])
+	if not ofs.open(argv[2]):
+		OEThrow.Fatal("Unable to open %s for writing" % argv[2])
+
+	print("Input index. 0: Aromatic, 1: Hydrophobic, 2: Polar Negative, 3: Polar Positive, 4: Polar Uncharged")
+	fragmentIndex=input()
+	m=Chem.MolFromMol2File(argv[1])
+	patt=Chem.MolFromSmarts("[Au]")
+
+	prepareAromatic()
+
+	fragmentFile=open(libraryFile[fragmentIndex],"r")
+	line=fragmentFile.readline()
+	
+	i=1
 
 	while True:
 		if not line:
@@ -85,16 +82,22 @@ def main(argv=[__name__]):
 
 		oemol=OEMol()
 		omega = OEOmega()
-		omega.SetMaxConfs(1)
+		omega.SetMaxConfs(50)
 		omega.SetStrictStereo(False)	
 		if OESmilesToMol(oemol,smiles):
 			print oemol
-
+		oemol.SetTitle("molecule_"+str(i))
 		if omega(oemol):
 			OEWriteMolecule(ofs, oemol)
 		i=i+1
-		line=f.readline()
+		line=fragmentFile.readline()
 	
 if __name__ == "__main__":
     sys.exit(main(sys.argv))
+
+
+
+
+
+
 
